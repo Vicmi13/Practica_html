d20e18724a608b175d1b0aaefcea9544ccf12916
Miguel Torres <vicmmi.13@gmail.com>
2017-10-04 19:12:44 -0500

    Se solucionan issues de peticiones XHR

--- js/ajax.js
+++ js/ajax.js
@@ -8,13 +8,28 @@ var drawTasks = function() {
 	} else {
 		var contentToAdd = '';
 		for (var i = 0; i < tasks.length; i++) {
-			contentToAdd.innerHTML = '<li class="task-item">' + tasks[i].name + '<button class="deleteTask" data-task-id="' + tasks[i].id + '">Eliminar</button></li>'
+            contentToAdd += '<li class="task-item">' + tasks[i].name + '<button class="deleteTask" data-task-id="' + tasks[i].id + '">Eliminar</button></li>'
 		}
 		$('#taskContainer').append(contentToAdd);
 	}
 }
 
+var getTasks = function (){
+      var XHR = new XMLHttpRequest();
+    XHR.open("GET", "http://localhost:8000/api/tasks", true);
+    XHR.setRequestHeader("Content-Type", "application/json");
 
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState === 4 && XHR.status === 200) {
+            console.log('XHR.responseText' + XHR.responseText);
+            tasks = JSON.parse(XHR.responseText);
+            drawTasks();
+        } else if (XHR.readyState === 4 && XHR.status === 404) {
+            console.log("Página no encontrada");
+        }
+    }
+     XHR.send(JSON.stringify({"name": name}));
+}
 
 var createTask = function (name) {
     var XHR = new XMLHttpRequest();
@@ -22,8 +37,8 @@ var createTask = function (name) {
     XHR.setRequestHeader("Content-Type", "application/json");
 
     XHR.onreadystatechange = function () {
-        if (XHR.readyState === 4 && XHR.status === 201) {
-            console.log(XHR.responseText);
+        if (XHR.readyState === 4 && XHR.status==201 ) {
+            console.log('XHR.responseText' + XHR.responseText);
             tasks.push(JSON.parse(XHR.responseText));
             drawTasks();
         } else if (XHR.readyState === 4 && XHR.status === 404) {
@@ -40,7 +55,7 @@ var deleteTask = function (id) {
     XHR.setRequestHeader("Content-Type", "application/json");
 
     XHR.onreadystatechange = function () {
-        if (XHR.readyState === 4) {
+        if (XHR.readyState === 4 && XHR.status === 204) {
             console.log("tasks deleted");
             getTasks();
         } else if (XHR.readyState === 4 && XHR.status === 404) {
@@ -51,8 +66,18 @@ var deleteTask = function (id) {
     XHR.send();
 }
 
+
 document.getElementById("sendNewTask").addEventListener("click", function (event) {
     event.preventDefault();
+    createTask(document.getElementById("newTaskName").value);
 });
 
-createTask(document.getElementById("newTaskName").value);
+
+getTasks();
+
+
+$(document).on('click', '.deleteTask', function(){
+    console.log('entra a delete')
+    var id = this.data('taskId');
+    deleteTask(id);
+});
